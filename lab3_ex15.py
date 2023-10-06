939e4149c0d66e7a7c80f3b2a9e422b062840d66
Gulzaaat <145113506+Gulzaaat@users.noreply.github.com>
2023-10-06 23:42:31 +0600

    Delete lab3_ex15.py

--- lab3_ex15.py
+++
@@ -1,8 +0,0 @@
-# «Кегельбан»
-a, b = [int(s) for s in input().split()]
-c = ['I'] * a
-for i in range(b):
-    l, r = [int(s) for s in input().split()]
-    for j in range(l - 1, r):
-        c[j] = '.'
-print(''.join(c))
