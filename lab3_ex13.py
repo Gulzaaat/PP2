15f35617340e3a9fe6c811218c32228f031fa3e7
Gulzaaat <145113506+Gulzaaat@users.noreply.github.com>
2023-10-06 23:42:59 +0600

    Delete lab3_ex13.py

--- lab3_ex13.py
+++
@@ -1,8 +0,0 @@
-# «Количество совпадающих пар»
-a = [int(s) for s in input().split()]
-cnt = 0
-for i in range(len(a)):
-    for j in range(i+1, len(a)):
-        if a[i] == a[j]:
-            cnt+=1
-print(cnt)
