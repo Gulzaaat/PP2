517de7fe43dbce9bbd2d3babec542adac5705f4a
Gulzaaat <145113506+Gulzaaat@users.noreply.github.com>
2023-10-06 23:43:27 +0600

    Delete lab3_ex4.py

--- lab3_ex4.py
+++
@@ -1,6 +0,0 @@
-# «Соседи одного знака»
-a = [int(i) for i in input().split()]
-for i in range(len(a) - 1):
-    if (a[i] < 0 and a[i+1] < 0) or (a[i] > 0 and a[i+1] > 0):
-        print(a[i], a[i+1])
-        break
