1d0a8c091d0dfe712e7218b67b62bda9ebda84a3
Gulzaaat <145113506+Gulzaaat@users.noreply.github.com>
2023-10-06 23:42:14 +0600

    Delete lab3_ex12.py

--- lab3_ex12.py
+++
@@ -1,8 +0,0 @@
-# «Вставить элемент»
-a = [int(s) for s in input().split()]
-b, c = [int(s) for s in input().split()]
-a.append(0)
-for i in range(len(a) - 1, b, -1):
-    a[i] = a[i-1]
-a[b] = c
-print(*a)
