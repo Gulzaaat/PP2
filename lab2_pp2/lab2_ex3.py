8ad7edf61e5d080079b0c51c8b1236a7a79dde9d
Gulzaaat <145113506+Gulzaaat@users.noreply.github.com>
2023-10-06 15:39:20 +0600

    lab2#

---
+++ lab2_pp2/lab2_ex3.py
@@ -0,0 +1,9 @@
+x1 = int(input())
+x2 = int(input())
+x3 = int(input())
+x4 = int(input())
+
+if (x1 + x2) % 2 == (x3 + x4) % 2:
+    print('YES')
+else:
+    print('NO')
---
+++ lab2_pp2/lab2_ex5.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if ((a < b) and (a < c)):
+    print(a)
+elif (b < a) and (b < c):
+    print(b)
+elif (c < a) and (c < b):
+    print(c)
+elif (a < c and a==b) or (a < b and a == c):
+    print(a)
+elif (b < c and b == a) or (b < a and b == c):
+    print(b)
+else:
+    print(c)
