0dda199dc9d69bdb1bb0f3635e643eba46928e3f
Gulzaaat <145113506+Gulzaaat@users.noreply.github.com>
2023-10-26 17:43:58 +0600

    Update json_1.py

--- lab6_pp2/json/json_1.py
+++ lab6_pp2/json/json_1.py
@@ -1,16 +1,13 @@
 import json
 
-with open('sample-data.json') as json_file:
-	data = json.load(json_file)
-
-print('Interface Status')
-print('='*84)
-names = ['DN', 'Description', 'Speed', 'MTU']
-form = "{:<47}  {:<15}  {:<10}  {:<6}"
-print(form.format(*names))
-print('-'*47 + '  ' + '-'*15 + '  ' + '-'*10 + '  ' + '-'*6)
-n = data["totalCount"]
-
-for i in data['imdata']:
-	dat = i['l1PhysIf']['attributes']
-	print(form.format(*(dat['dn'], '' if dat.get('description') == None else dat['description'], dat['speed'], dat['mtu'])))
+file = open('sample-data.json', 'r')
+js = json.load(file)
+print("Interface Status\n==============================================================================================")
+print("DN                                                 Description                  Speed      MTU ")
+print("-------------------------------------------------- ---------------------------  ------   ------")
+for i in js['imdata']:
+    print(i['l1PhysIf']['attributes']['dn'], end ="                       \t")
+    print(i['l1PhysIf']['attributes']['descr'], end= "  \t")
+    print( i['l1PhysIf']['attributes']['speed'], end= "   ")
+    print( i['l1PhysIf']['attributes']['mtu'], end= '\n')
+file.close()
